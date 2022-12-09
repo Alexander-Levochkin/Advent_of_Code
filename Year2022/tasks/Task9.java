9a2d17fa53b8b4eaa9e2d91d1a8af6d070c021c7
Alexandr <lev5sanek@gmail.com>
2022-12-09 16:39:05 +0300

    Year 2022 Day 9 pre-solution

--- Year2022/tasks/Task9.java
+++ Year2022/tasks/Task9.java
@@ -1,26 +1,142 @@
 package Year2022.tasks;
 
-import helper.ToListScanner;
+import Year2022.scanner.InputScanner;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <a href="https://adventofcode.com/2022/day/9">Task description</a>
  */
 public class Task9 {
-    private final List<String> input;
 
-    public Task9() throws IOException {
-        String path = getClass().getSimpleName();
-        input = ToListScanner.toString(path);
+    public int part1() throws IOException {
+        Rope rope = new Rope(2);
+        for (String line : Files.readAllLines(InputScanner.getPath(getClass().getSimpleName()))) {
+            String[] args = line.split(" ");
+            for (int i = 0; i < Integer.parseInt(args[1]); i++)
+                rope.move(args[0]);
+        }
+        return rope.getTail().markedCells.size();
     }
 
-    public int part1() {
-        return -1;
+    public int part2() throws IOException {
+        Rope rope = new Rope(10);
+        for (String line : Files.readAllLines(InputScanner.getPath(getClass().getSimpleName()))) {
+            String[] args = line.split(" ");
+            for (int i = 0; i < Integer.parseInt(args[1]); i++)
+                rope.move(args[0]);
+        }
+        return rope.getTail().markedCells.size();
     }
 
-    public int part2() {
-        return -1;
+    private static class Rope {
+        int xHead = 1;
+        int yHead = 1;
+        Rope tail;
+        final Set<Coordinate> markedCells = new HashSet<>();
+
+        public Rope(int size) {
+            Rope current = this;
+            for (int i = 0; i < size-1; i++) {
+                Rope rope = new Rope();
+                current.setTail(rope);
+                current = rope;
+            }
+            current.setTail(null);
+        }
+
+        private Rope() {
+            markedCells.add(new Coordinate(1, 1));
+        }
+
+        public void setTail(Rope tail) {
+            this.tail = tail;
+        }
+
+        public void move(String direction) {
+            moveHead(direction);
+            moveTail();
+        }
+
+        private void moveTail() {
+            if (tail != null) {
+                boolean moveX = Math.abs(xHead - tail.xHead) == 2;
+                boolean moveY = Math.abs(yHead - tail.yHead) == 2;
+
+                if ((moveX && Math.abs(yHead - tail.yHead) == 1) || (moveY && Math.abs(xHead - tail.xHead) == 1)) {
+                    tail.xHead += xHead < tail.xHead ? -1 : 1;
+                    tail.yHead += yHead < tail.yHead ? -1 : 1;
+                } else if (moveX && !moveY) tail.xHead += xHead < tail.xHead ? -1 : 1;
+                else if (!moveX && moveY) tail.yHead += yHead < tail.yHead ? -1 : 1;
+
+                tail.markedCells.add(new Coordinate(tail.xHead, tail.yHead));
+                tail.moveTail();
+            }
+        }
+
+        private void moveHead(String direction) {
+            switch (direction) {
+                case "L" -> xHead--;
+                case "R" -> xHead++;
+                case "U" -> yHead++;
+                case "D" -> yHead--;
+            }
+            markedCells.add(new Coordinate(xHead, yHead));
+        }
+
+        public Rope getTail() {
+            return tail == null ? this : tail.getTail();
+        }
+
+        private record Coordinate(int x, int y) {}
     }
+
+/*    private static class Rope {
+        int xHead = 1;
+        int yHead = 1;
+        int xTail = 1;
+        int yTail = 1;
+
+        final Set<Coordinate> markedCells = new HashSet<>();
+
+        public Rope() {
+            markedCells.add(new Coordinate(xTail, yTail));
+        }
+
+        public void move(Direction direction) {
+            moveHead(direction);
+            moveTail();
+            markedCells.add(new Coordinate(xTail, yTail));
+*//*            System.out.println("Head: " + xHead + " " + yHead);
+            System.out.println("Tail: " + xTail + " " + yTail);
+            System.out.println();*//*
+        }
+
+        private void moveTail() {
+            boolean moveX = Math.abs(xHead - xTail) == 2;
+            boolean moveY = Math.abs(yHead - yTail) == 2;
+
+            if ((moveX & Math.abs(yHead - yTail) == 1) || (moveY && Math.abs(xHead - xTail) == 1)) {
+                xTail += xHead < xTail ? -1 : 1;
+                yTail += yHead < yTail ? -1 : 1;
+            } else if (moveX && !moveY) xTail += xHead < xTail ? -1 : 1;
+            else if (!moveX && moveY) yTail += yHead < yTail ? -1 : 1;
+        }
+
+        private void moveHead(Direction direction) {
+            switch (direction) {
+                case L -> xHead--;
+                case R -> xHead++;
+                case U -> yHead++;
+                case D -> yHead--;
+            }
+        }
+
+        private record Coordinate(int x, int y) {}
+    }*/
 }
