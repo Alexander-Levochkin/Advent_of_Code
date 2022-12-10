96d227543aa3d54a643847261744462fe3bd1f22
Alexandr <lev5sanek@gmail.com>
2022-12-11 01:05:06 +0300

    Year 2022 Day 10 solution

--- Year2022/Main2022.java
+++ Year2022/Main2022.java
@@ -1,13 +1,13 @@
 package Year2022;
 
-import Year2022.tasks.Task8;
+import Year2022.tasks.*;
 
 import java.io.IOException;
 
 public class Main2022 {
     public static void main(String[] args) throws IOException {
-        var task = new Task8();
-        System.out.println(task.part1());
+        var task = new Task10();
+//        System.out.println(task.part1());
         System.out.println(task.part2());
     }
 }
